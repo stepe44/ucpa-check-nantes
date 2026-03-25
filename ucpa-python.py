284cc4f9511eb2ccc90968c98f3c76f0ed7ded3f
stepe44 <stephane.evain@gmail.com>
2026-03-25 15:18:37 +0100

    Refactor ucpa-python.py by re-adding imports and functions

--- ucpa-python.py
+++ ucpa-python.py
@@ -69,8 +69,8 @@ def send_final_notification(liste_alertes):
     corps = ""
     for a in liste_alertes:
         date_rel = formater_date_relative(a['date'])
-        corps += (f"🏋️ *{a['nom']}*\n📅 {date_rel} ⏰ {a['horaire']} (🔥 {a['places']} place(s))\n")
-    
+        corps += (f"🏋️ *{a['nom']}*\n📅 {date_rel} ⏰ {a['horaire']} ({a['places']} place(s))\n")
+    #   corps += (f"🏋️ *{a['nom']}*\n📅 {date_rel}\n⏰ {a['horaire']}\n🔥 {a['places']} place(s)!\n-------------------\n") #
     msg_whatsapp = f"{titre}\n{corps}🔗 {URL_UCPA}"
     
     if GREEN_API_URL and WHATSAPP_CHAT_ID:
